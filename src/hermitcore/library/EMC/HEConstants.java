dc5bd52bba9e3110a02d2ccebff0f95085c60818
Perry Berman <RealCloudCraft@users.noreply.github.com>
2015-11-19 10:22:20 -0700

    Added Klein Star Siebens max EMC storage

--- src/hermitcore/library/EMC/HEConstants.java
+++ src/hermitcore/library/EMC/HEConstants.java
@@ -2,5 +2,5 @@ package hermitcore.library.EMC;
 
 public class HEConstants {
 
-	public static final int[] MAX_KLEIN_EMC = new int[] {204800000};
+	public static final int[] MAX_KLEIN_EMC = new int[] {204800000, 819200000};
 }
